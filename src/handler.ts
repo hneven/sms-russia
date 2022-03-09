63c74d7ac012dc526cf06fb7fa1d21a502d81d61
Helge.Neven <helge.neven@otto.de>
2022-03-09 13:21:59 +0100

    chore: deletes whitespace

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,7 @@
 import {getPhoneNumber} from "./apis/cellphoneApi";
 import {sendSmsViaRapid} from "./apis/rapidApi";
 
+
 getPhoneNumber()
     .then(phoneNumber => sendSmsViaRapid(phoneNumber))
     .catch(err => console.log(err));
